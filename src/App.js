48d1a59b47a41bbf713515b17ea58a8041ba6837
JonasDeOliveira <jonas.doliver@gmail.com>
2022-10-06 09:52:16 -0300

    boilerplatecode

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const App = props => {
+
+    return (
+        <>
+        <Text>Inicio da Aplicação</Text>
+        </>
+    );
+};
+
+export default App;
